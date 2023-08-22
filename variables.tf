c5fd0a59b9dc025abec22cd0fefc560be149c612
svenkatesh9666 <137752334+svenkatesh9666@users.noreply.github.com>
2023-08-22 22:33:12 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,26 @@
+ /* Demo VPC varaibles file. Make sure you replace key_name value
+ with your key name which  you have in given aws_region.
+*/
+variable "aws_region" {
+  default = "ap-south-1"
+}
+variable "vpc_cidr" {
+  default = "172.0.0.0/24"
+}
+variable "subnets_cidr" {
+  type    = list(string)
+  default = ["172.0.0.0/25", "172.0.0.128/25"]
+}
+variable "availability_zones" {
+  type    = list(string)
+  default = ["ap-south-1a", "ap-south-1b", ap-south-1c]
+}
+variable "ubuntuami" {
+  default = "ami-0f5ee92e2d63afc18"
+}
+variable "redhatami" {
+  default = "ami-008b85aa3ff5c1b02"
+}
+variable "key_name" {
+  default = "devops"
+}
