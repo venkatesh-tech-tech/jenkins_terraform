4dcc1cee9883ee23df87c0cece194d151943c719
svenkatesh9666 <137752334+svenkatesh9666@users.noreply.github.com>
2023-08-24 10:23:00 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,9 @@
 # Provider
 provider "aws" {
-  region = "ap-south-1"
+  region     = "ap-south-1"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
 }
 # VPC
 resource "aws_vpc" "vpc" {
